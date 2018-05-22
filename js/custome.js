6469cd5ed44e39ebd34dec9342cf2524b9b6ee60
sayed mokituzzaman <34116580+Mokituzzaman@users.noreply.github.com>
2018-05-22 17:30:58 +0600

    Add files via upload

---
+++ js/custome.js
@@ -0,0 +1,11 @@
+jQuery(document).ready(function(){
+  jQuery(".owl-carousel").owlCarousel({
+  	"items":4,
+  	"autoplay":true,
+ 	'autoplayTimeout':1500,
+  	'autoplayHoverPause':true,
+  	'loop':true,
+
+  
+  });
+});
